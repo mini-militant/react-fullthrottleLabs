9373c38fdc61b972c9fd5a4fe6f004304e1ed0ec
mini-militant <shailesh.coolcalm007@gmail.com>
2019-10-11 10:34:52 +0530

    2 files modified

--- Hello.js
+++ Hello.js
@@ -1,6 +1,20 @@
 import React from 'react';
 
 class Hello extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      loanAmount:''
+    }
+  }
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
   render(){
   return(
     <div>
@@ -14,8 +28,11 @@ class Hello extends React.Component{
     <input type="text" name="name" />
   </label><br/>
   <input type="submit" value="Submit" />
-</form>
-    
+</form> 
+  <div class="slidecontainer">
+  <input type="range" min="1" max="100" value="50" class="slider" id="myRange"/>
+  <p>Value: <span id="demo"></span></p>
+  </div>   
     </div>
   )
 }
