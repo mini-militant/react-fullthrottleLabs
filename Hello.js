456f62ca290ad211b5cbb9bc639cd9ce97a9d342
mini-militant <shailesh.coolcalm007@gmail.com>
2019-10-12 16:35:39 +0530

    1 file modified

--- Hello.js
+++ Hello.js
@@ -1,41 +1,59 @@
-import React from 'react';
+import React from "react";
 
-class Hello extends React.Component{
-  constructor(props){
+class Hello extends React.Component {
+  constructor(props) {
     super(props);
-    this.state={
-      loanAmount:''
-    }
+    this.state = {
+      loanAmount: 500
+    };
   }
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
+  handleChange = event => {
+    this.setState({ loanAmount: event.target.value });
+  };
 
   handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
+    alert("A name was submitted: " + this.state.value);
     event.preventDefault();
   }
-  render(){
-  return(
-    <div>
-    <form>
-  <label>
-    Loan Amount:
-    <input type="text" name="name" />
-  </label><br/>
-  <label>
-    Loan Duration:
-    <input type="text" name="name" />
-  </label><br/>
-  <input type="submit" value="Submit" />
-</form> 
-  <div class="slidecontainer">
-  <input type="range" min="1" max="100" value="50" class="slider" id="myRange"/>
-  <p>Value: <span id="demo"></span></p>
-  </div>   
-    </div>
-  )
-}
+  render() {
+    return (
+      <div>
+        <form>
+          <label>
+            Loan Amount:
+            <input
+              type="text"
+              name="name"
+              value={this.state.loanAmount}
+              onChange={this.handleChange}
+            />
+          </label>
+          <br />
+          <label>
+            Loan Duration:
+            <input type="text" name="name" />
+          </label>
+          <br />
+          <input type="submit" value="Submit" />
+        </form>
+
+        <div className="slidecontainer">
+          <input
+            type="range"
+            min="500"
+            max="5000"
+            value={this.state.loanAmount}
+            className="slider"
+            id="myRange"
+            onChange={this.handleChange}
+          />
+          <p>
+            Value: <span id="demo" />
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Hello;
