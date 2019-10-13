e17ec1bf3f189eeb277c64c199489b0efafd713d
mini-militant <shailesh.coolcalm007@gmail.com>
2019-10-13 18:43:41 +0530

    1 file modified

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import LoanCalculator from '../Components/LoanCalculator';
+import LoanCalculator from './Components/LoanCalculator';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/style.css';
 
